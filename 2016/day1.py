ff034962c8553e620044c4b1d3089b7a057342e8
Justin Leong <justinleong16@gmail.com>
2017-11-30 11:26:18 -0800

    Finish Day 1 Part 1

---
+++ 2016/day1.py
@@ -0,0 +1,22 @@
+INPUT = "R2, L1, R2, R1, R1, L3, R3, L5, L5, L2, L1, R4, R1, R3, L5, L5, R3, L4, L4, R5, R4, R3, L1, L2, R5, R4, L2, R1, R4, R4, L2, L1, L1, R190, R3, L4, R52, R5, R3, L5, R3, R2, R1, L5, L5, L4, R2, L3, R3, L1, L3, R5, L3, L4, R3, R77, R3, L2, R189, R4, R2, L2, R2, L1, R5, R4, R4, R2, L2, L2, L5, L1, R1, R2, L3, L4, L5, R1, L1, L2, L2, R2, L3, R3, L4, L1, L5, L4, L4, R3, R5, L2, R4, R5, R3, L2, L2, L4, L2, R2, L5, L4, R3, R1, L2, R2, R4, L1, L4, L4, L2, R2, L4, L1, L1, R4, L1, L3, L2, L2, L5, R5, R2, R5, L1, L5, R2, R4, R4, L2, R5, L5, R5, R5, L4, R2, R1, R1, R3, L3, L3, L4, L3, L2, L2, L2, R2, L1, L3, R2, R5, R5, L4, R3, L3, L4, R2, L5, R5".split(", ")
+
+coord = [0, 0]
+direction = 0
+MOVE = [[0, 1], [1, 0], [0,-1], [-1,0]]
+visited = set()
+
+for cmd in INPUT:
+    if cmd[0] == 'R':
+        direction = (direction + 1) % 4
+    else:
+        direction = (direction - 1 + 4) % 4
+    print direction
+    dist = int(cmd[1:])
+    for _ in xrange(dist):
+        coord[0] = coord[0] + MOVE[direction][0]
+        coord[1] = coord[1] + MOVE[direction][1]
+    print coord
+
+
+
+print abs(coord[0]) + abs(coord[1])
