9ceae8fbd0b8cbbbec6a2e39ad723fe3d69d7df8
Justin Leong <justinleong16@gmail.com>
2017-12-02 20:38:05 -0800

    Update to prep for day 3

---
+++ 2017/3_1.py
@@ -0,0 +1,12 @@
+from collections import *
+import itertools
+import sys
+
+def main():
+    ans = 0
+    for line in sys.stdin:
+        print line
+        # a = list(map(int, line.strip().split()))
+    print ans
+
+main()
